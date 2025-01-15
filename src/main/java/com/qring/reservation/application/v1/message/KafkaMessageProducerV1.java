c6e2e15dfb732b56895d50b94d7eb95bb248d235
오세창 <114675677+sepang-pang@users.noreply.github.com>
2025-01-15 22:07:11 +0900

    [ refactor ] 카프카 메시지 발행 타입 변경  ( #17 )

--- src/main/java/com/qring/reservation/application/v1/message/KafkaMessageProducerV1.java
+++ src/main/java/com/qring/reservation/application/v1/message/KafkaMessageProducerV1.java
@@ -2,13 +2,13 @@ package com.qring.reservation.application.v1.message;
 
 import com.qring.reservation.infrastructure.messaging.dto.CreateReservationMessageDTOV1;
 import com.qring.reservation.infrastructure.messaging.dto.ReservationSendUserInfoEventDTOV1;
-import com.qring.reservation.infrastructure.messaging.dto.ReservationUpdateEventDTOV1;
+import com.qring.reservation.infrastructure.messaging.dto.UpdateReservationMessageDTOV1;
 
 public interface KafkaMessageProducerV1 {
 
     void publishReservationCreateEvent(CreateReservationMessageDTOV1 message);
 
-    void publishReservationUpdateEvent(ReservationUpdateEventDTOV1.Reservation reservation);
+    void publishReservationUpdateEvent(UpdateReservationMessageDTOV1 message);
 
     void publishUserInfoSendEvent(ReservationSendUserInfoEventDTOV1.User user);
 
--- src/main/java/com/qring/reservation/infrastructure/messaging/KafkaMessageProducerImplV1.java
+++ src/main/java/com/qring/reservation/infrastructure/messaging/KafkaMessageProducerImplV1.java
@@ -3,7 +3,7 @@ package com.qring.reservation.infrastructure.messaging;
 import com.qring.reservation.application.v1.message.KafkaMessageProducerV1;
 import com.qring.reservation.infrastructure.messaging.dto.CreateReservationMessageDTOV1;
 import com.qring.reservation.infrastructure.messaging.dto.ReservationSendUserInfoEventDTOV1;
-import com.qring.reservation.infrastructure.messaging.dto.ReservationUpdateEventDTOV1;
+import com.qring.reservation.infrastructure.messaging.dto.UpdateReservationMessageDTOV1;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
@@ -19,9 +19,9 @@ public class KafkaMessageProducerImplV1 implements KafkaMessageProducerV1 {
         kafkaTemplate.send("reservation-create-event-topic", message);
     }
 
-    public void publishReservationUpdateEvent(ReservationUpdateEventDTOV1.Reservation reservation) {
+    public void publishReservationUpdateEvent(UpdateReservationMessageDTOV1 message) {
 
-        kafkaTemplate.send("reservation-update-event-topic", reservation);
+        kafkaTemplate.send("reservation-update-event-topic", message);
     }
 
     public void publishUserInfoSendEvent(ReservationSendUserInfoEventDTOV1.User user) {
