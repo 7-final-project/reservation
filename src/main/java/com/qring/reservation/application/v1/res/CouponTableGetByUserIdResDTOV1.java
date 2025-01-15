ea8ec7506189a3091b1d362ba9cad2c2892821b2
오세창 <114675677+sepang-pang@users.noreply.github.com>
2025-01-15 21:30:48 +0900

    [ refactor ] CouponTableGetByUserIdResDTOV1 구조 간소화 및 쿠폰 ID 확인 메서드 추가 ( #17 )
    - 사용되지 않는 필드 및 어노테이션 제거
    
    - `couponId`를 확인할 수 있는 로직 구현

--- src/main/java/com/qring/reservation/application/v1/res/CouponTableGetByUserIdResDTOV1.java
+++ src/main/java/com/qring/reservation/application/v1/res/CouponTableGetByUserIdResDTOV1.java
@@ -1,43 +1,46 @@
 package com.qring.reservation.application.v1.res;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Getter
-@Builder
 @NoArgsConstructor
-@AllArgsConstructor
 public class CouponTableGetByUserIdResDTOV1 {
 
     private Set<UserCoupon> userCouponSet;
 
     @Getter
-    @Builder
     @NoArgsConstructor
-    @AllArgsConstructor
     public static class UserCoupon {
 
-        private String useStatus;
         private Coupon coupon;
 
         @Getter
-        @Builder
         @NoArgsConstructor
-        @AllArgsConstructor
         public static class Coupon {
 
+            // NOTE: 현재 coupon 의 ID 만 필요하므로, id 필드만 포함합니다.
             private Long id;
-            private String name;
-            private LocalDateTime openAt;
-            private LocalDateTime expiredAt;
-            private String couponStatus;
-            private String issuanceStatus;
 
         }
+    }
+
+    /**
+     * 특정 couponId가 userCouponSet에 존재하는지 확인합니다.
+     *
+     * @param couponId 확인하려는 쿠폰 ID
+     * @return 해당 쿠폰이 존재하면 true, 그렇지 않으면 false
+     */
+    public boolean hasCoupon(Long couponId) {
+        return  getCouponIdSet().contains(couponId);
+    }
+
+    private Set<Long> getCouponIdSet() {
+        return this.userCouponSet.stream()
+                .map(userCoupon -> userCoupon.getCoupon().getId())
+                .collect(Collectors.toSet());
     }
 }
