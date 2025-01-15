2a987dc8346f7283493c4f4dd866eddbbb424bc8
오세창 <114675677+sepang-pang@users.noreply.github.com>
2025-01-15 22:45:08 +0900

    [ refactor ] 사용자 정보 관련 DTO 클래스 네이밍 및 구조 변경 ( #17 )

---
+++ src/main/java/com/qring/reservation/infrastructure/messaging/dto/SendUserInfoMessageDTOV1.java
@@ -0,0 +1,41 @@
+package com.qring.reservation.infrastructure.messaging.dto;
+
+import com.qring.reservation.application.v1.res.UserGetByIdResDTOV1;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SendUserInfoMessageDTOV1 {
+
+    private User user;
+
+    public static SendUserInfoMessageDTOV1 of(UserGetByIdResDTOV1 dto) {
+        return SendUserInfoMessageDTOV1.builder()
+                .user(User.from(dto))
+                .build();
+    }
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class User {
+
+        private Long id;
+        private String username;
+        private String slackEmail;
+
+        public static User from(UserGetByIdResDTOV1 dto) {
+            return User.builder()
+                    .id(dto.getUser().getId())
+                    .username(dto.getUser().getUsername())
+                    .slackEmail(dto.getUser().getSlackEmail())
+                    .build();
+        }
+    }
+}
