8b272d731624fa972d9c87543bfd9ed1218f322f
오세창 <114675677+sepang-pang@users.noreply.github.com>
2025-01-21 22:08:44 +0900

    [ refactor ] 카프카 프로듀서 v3 추가 : 다중파티션 설정 ( #21 )

---
+++ src/main/java/com/qring/reservation/infrastructure/config/KafkaProducerConfigV3.java
@@ -0,0 +1,46 @@
+package com.qring.reservation.infrastructure.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@ConditionalOnProperty(name = "version.v3.enabled", havingValue = "true", matchIfMissing = true)
+@Slf4j(topic = "KafkaProducerConfigV3 : 다중 파티션")
+public class KafkaProducerConfigV3 {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String kafkaServer;
+
+    @Bean
+    public ProducerFactory<String, Object> producerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaServer);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+    @Bean
+    public NewTopic reservationCreateEventTopic() {
+        return new NewTopic("reservation-create-event-topic-v3", 41, (short) 1);
+    }
+}
